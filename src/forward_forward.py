c86f480c463907c5d7a5c083361355d18d0c4164
Chanjeong Park <ckswjd99@snu.ac.kr>
2023-11-12 11:46:57 +0900

    ff base

---
+++ src/forward_forward.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BlockBase(nn.Module):
+    def __init__(self, in_dim, out_dim, normalize_input, ff, bias=False):
+        super().__init__()
+        self.fc = nn.Linear(in_dim, out_dim, bias=bias)
+        self.relu = nn.ReLU(True)
+        self.normalize_input = normalize_input
+        self.ff = ff
+
+    def forward(self, x):
+        if self.normalize_input:
+            x = F.normalize(x, dim=1)
+        
+        x = self.fc(x)
+        self.x = x
+
+        if self.ff:
+            return self.relu(x).detach()
+        return self.relu(x)
+
+class BlockMLP(nn.Module):
+    def __init__(self, in_dim, out_dim, normalize_input, ff, bias=False):
+        super().__init__()
+        self.fc1 = nn.Linear(in_dim, out_dim, bias=bias)
+        self.fc2 = nn.Linear(out_dim, out_dim, bias=bias)
+        self.relu = nn.ReLU(True)
+        self.normalize_input = normalize_input
+        self.ff = ff
+
+    def forward(self, x):
+        if self.normalize_input:
+            x = F.normalize(x, dim=1)
+        
+        x = self.fc(x)
+        self.x = x
+
+        if self.ff:
+            return self.relu(x).detach()
+        return self.relu(x)
+
+class FFBase(nn.Module):
+    def __init__(self, dims, ff=True):
+        super().__init__()
+
+        blocks = []
+        blocks.append(BlockBase(dims[0], dims[1], False, ff))
+        for i in range(len(dims[1:-1])):
+            blocks.append(BlockBase(dims[i+1], dims[i+2], True, ff))
+        
+        # just for print
+        self.blocks = nn.Sequential(*blocks)
+        self.n_blocks = len(blocks)
+        self.ff = ff
+
+    def forward(self, x, cat=True):
+        x = self.blocks(x)
+
+        if not self.ff:
+            return x
+        
+        xs = [b.x for b in self.blocks.children()]
+
+        if not cat:
+            return xs
+        return torch.stack(xs, dim=1)
+        
